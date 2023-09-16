9abe008645bb7141f7f2bbac00566ba5f7a565ca
Noah <noahhughesph@gmail.com>
2023-09-16 16:00:10 -0400

    all tests passing

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,25 @@
-import React from "react";
-import { directors } from "../data";
+import React from 'react';
+import { directors } from '../data';
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map((director) => {
+        return (
+          <div key={director.name}>
+            <h3>Name: {director.name}</h3>
+            <p>Movies: </p>
+            <ul>
+              {director.movies.map((movie) => {
+                return <li key={director.name + movie}>{movie}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Directors;
